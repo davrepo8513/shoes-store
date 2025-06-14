db3e242fd5623e65bcaf8107525bb5362a04ddba
divyam <tyagidav97@gmail.com>
2025-06-14 22:01:30 +0000

    resolved build isssue

--- src/components/ProductListItem/ProductListItem.js
+++ src/components/ProductListItem/ProductListItem.js
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Heart, ShoppingCart, Star, Eye } from 'lucide-react';
 import { motion } from 'framer-motion';
